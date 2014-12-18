c1e1ebbbd8151826bf118c8e46a79a6afd5fc781
Stan Lewis <slewis@fusesource.com>
2014-12-18 15:34:39 -0500

    Add lodash include

--- templates/plugins/includes.ts
+++ templates/plugins/includes.ts
@@ -4,4 +4,5 @@
 /// <reference path="../libs/hawtio-core-dts/angular-resource.d.ts"/>
 /// <reference path="../libs/hawtio-core-dts/hawtio-core.d.ts"/>
 /// <reference path="../libs/hawtio-core-dts/angular-route.d.ts"/>
+/// <reference path="../libs/hawtio-core-dts/lodash.d.ts"/>
 /// <reference path="../libs/hawtio-core-dts/hawtio-core-navigation.d.ts"/>
