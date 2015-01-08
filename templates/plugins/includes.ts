a35770e6cb419a06c8f9d462f64595819468025d
Stan Lewis <slewis@fusesource.com>
2015-01-08 10:37:53 -0500

    Just reference the defs.d.ts from hawtio-utilities

--- templates/plugins/includes.ts
+++ templates/plugins/includes.ts
@@ -1,8 +1,1 @@
-/// <reference path="../libs/hawtio-core-dts/logger.d.ts"/>
-/// <reference path="../libs/hawtio-core-dts/jquery.d.ts"/>
-/// <reference path="../libs/hawtio-core-dts/angular.d.ts"/>
-/// <reference path="../libs/hawtio-core-dts/angular-resource.d.ts"/>
-/// <reference path="../libs/hawtio-core-dts/hawtio-core.d.ts"/>
-/// <reference path="../libs/hawtio-core-dts/angular-route.d.ts"/>
-/// <reference path="../libs/hawtio-core-dts/lodash.d.ts"/>
-/// <reference path="../libs/hawtio-core-dts/hawtio-core-navigation.d.ts"/>
+/// <reference path="../libs/hawtio-utilities/defs.d.ts"/>
